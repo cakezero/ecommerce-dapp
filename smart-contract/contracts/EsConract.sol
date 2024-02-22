83c8d962a86548d3dd3ae0f66bb8bdd51cc63df1
cakezero <sareahj.com@gmail.com>
2024-02-22 21:50:31 +0100

    fix: chainlink vrf -> chainlink functions

--- smart-contract/contracts/EsConract.sol
+++ smart-contract/contracts/EsConract.sol
@@ -1,26 +1,18 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.20;
-import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
-import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
-import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import { ConfirmedOwner } from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
 import { IRouterClient } from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import { Client } from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
 import { IERC20 } from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.0/contracts/token/ERC20/IERC20.sol";
+import { FunctionsClient } from "@chainlink/contracts/src/v0.8/functions/dev/v1_0_0/FunctionsClient.sol";
+import { FunctionsRequest } from "@chainlink/contracts/src/v0.8/functions/dev/v1_0_0/libraries/FunctionsRequest.sol";
 
-contract EsContract is FunctionsClient, VRFConsumerBaseV2, ConfirmedOwner {
 
-	uint8 numWords = 1;
-	uint8 confirmations = 3;
-	uint32 callbackGasLimit = 100000;
-    uint64 subsId;
+contract EsContract is FunctionsClient, ConfirmedOwner {
 	
-    bytes32 keyHash;
-    VRFCoordinatorV2Interface COORDINATOR;
     IRouterClient public immutable ccipRouter;
     IERC20 public immutable usdc;
-
-    // 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c keyHash
 
     error NotEnoughFeesForGas(uint balance, uint fees);
     error NothingToWithdraw();
@@ -35,7 +27,6 @@ contract EsContract is FunctionsClient, VRFConsumerBaseV2, ConfirmedOwner {
     mapping (address => bool) public isMerchant;
     mapping (address => MerchantDetail) internal merchantDetail;
     mapping (address => uint256) internal MerchantPayment;
-    mapping (uint => s_requests) public reqs;
     mapping (uint256 => order) internal Order;
 
     struct MerchantDetail {
@@ -49,11 +40,6 @@ contract EsContract is FunctionsClient, VRFConsumerBaseV2, ConfirmedOwner {
         address[] UsdcContracts;
         uint[] prices;
     }
-    struct s_requests {
-        bool exists;
-        bool fulfilled;
-        uint256[] randomWords;
-    }
 
     address[] public merc;
     uint64[] public chain;
@@ -64,32 +50,11 @@ contract EsContract is FunctionsClient, VRFConsumerBaseV2, ConfirmedOwner {
     event MerchantRemoved(address indexed merchant);
     event MoneySent(address indexed reciever, uint amount);
     event Paid(address indexed sender, uint amount);
-    event RequestFulfilled(uint256 reqId, uint256[] randomWords);
 
 
-    constructor(address _ccipRouter, address _usdc, uint64 _subsId, bytes32 _keyHash, address _vrfCoordinator ) VRFConsumerBaseV2(_vrfCoordinator) ConfirmedOwner(msg.sender){
+    constructor(address _ccipRouter, address _usdc) ConfirmedOwner(msg.sender){
         usdc = IERC20(_usdc);
         ccipRouter = IRouterClient(_ccipRouter);
-        COORDINATOR = VRFCooordinatorV2Interface(_vrfCoordinator);
-        keyHash = _keyHash;
-        subsId = _subsId;
-    }
-
-    function generateId() internal returns (uint256 reqId) {
-		reqId = COORDINATOR.requestRandomWords(keyHash, subsId, confirmations, callbackGasLimit, numWords);
-        reqs[reqId] = s_requests({
-            randomWords: new uint256[](0),
-            fulfilled: false,
-            exists: true
-        });
-		return reqId;
-	}
-
-    function fulfillRandomWords(uint256 reqId, uint256[] memory randomWords) internal override {
-        require(reqs[reqId].exists == true, 'Id not found');
-        reqs[reqId].fulfilled = true;
-        reqs[reqId].randomWords = randomWords;
-        emit RequestFulfilled(reqId, randomWords);
     }
 
     function CreateMerchant(uint64 _chainSelector, address _UsdcContract) external {
@@ -101,17 +66,12 @@ contract EsContract is FunctionsClient, VRFConsumerBaseV2, ConfirmedOwner {
         emit MerchantCreated(msg.sender);
     }
 
-    function RemoveMerchant(address _merchant) external onlyOwner {
-        // merchants[_merchant] = false;
-        emit MerchantRemoved(_merchant);
-    }
-
-    function deposit(uint amount, uint[] memory prices, address[] memory _merchants) external payable {
-        uint Id = generateId();
-        uint id = reqs[Id].randomWords[0];
+    function deposit(uint amount, uint[] memory prices, address[] memory _merchants) external payable returns (bytes8 id) {
+        id = generateId();
         if (msg.value != amount) revert SendRequiredAmount();
+        address[] hu = new address[](_merchants.length);
         for (uint i = 0; i < _merchants.length; i++) {
-            merc.push(merchantDetail[_merchants[i]].merchant);
+            hu.push(merchantDetail[_merchants[i]].merchant);
             chain.push(merchantDetail[_merchants[i]].ChainSelector);
             cont.push(merchantDetail[_merchants[i]].UsdcContract);
         }
@@ -121,9 +81,11 @@ contract EsContract is FunctionsClient, VRFConsumerBaseV2, ConfirmedOwner {
             ChainSelectors: chain,
             UsdcContracts: cont
         });
+
+        return id;
     }
 
-    function PayMerchant(uint64 _ChainSelector, address _receiver, address _token) external onlyMerchant returns(bytes32 messageId) {
+    function PayMerchant(uint256 id) external returns(bytes32 messageId) {
         
     }
 
