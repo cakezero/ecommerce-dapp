a2f00110b54297b6c1b6b1305b3b15272aa02b6d
cakezero <sareahj.com@gmail.com>
2024-02-27 01:11:43 +0100

    feat: implemented cross-chain payment with ccip

--- smart-contract/contracts/EsConract.sol
+++ smart-contract/contracts/EsConract.sol
@@ -14,6 +14,10 @@ contract EsContract is FunctionsClient, ConfirmedOwner {
 
     IRouterClient public immutable ccipRouter;
     IERC20 public immutable usdc;
+    address public immutable usdcContract;
+    uint64 public immutable chainSelector;
+
+    IERC20 public linkToken;
 
     error NotEnoughFeesForGas(uint balance, uint fees);
     error NothingToWithdraw();
@@ -25,23 +29,25 @@ contract EsContract is FunctionsClient, ConfirmedOwner {
 
     mapping(address => uint256) internal commission;
     mapping(address => bool) public isMerchant;
-    mapping(bytes8 => order) internal Order;
+    mapping(bytes8 => order) external Order;
 
     struct order {
         address[] merchants;
         uint[] prices;
-        string trackingCode;
     }
 
     event CommissionPaid(address indexed receiver, uint amount);
     event MerchantCreated(address indexed merchant);
     event MerchantRemoved(address indexed merchant);
     event MoneySent(address indexed reciever, uint amount);
-    event Paid(address indexed sender, uint amount);
+    event Paid(bytes32indexed messageId, address indexed receiver, uint amount);
 
-    constructor(address _ccipRouter, address _usdc, address _functionsRouter) FunctionsClient(_functionsRouter) ConfirmedOwner(msg.sender) {
+    constructor(address _ccipRouter, address _usdc, address _functionsRouter, uint64 _chainSelector, address _linkToken) FunctionsClient(_functionsRouter) ConfirmedOwner(msg.sender) {
         usdc = IERC20(_usdc);
         ccipRouter = IRouterClient(_ccipRouter);
+        usdcContract = _usdc;
+        linkToken = IERC20(_linkToken);
+        chainSelector = _chainSelector
     }
 
     function generateId() internal view returns (bytes8 id) {
@@ -63,27 +69,51 @@ contract EsContract is FunctionsClient, ConfirmedOwner {
         id = generateId();
         if (msg.value != amount) revert SendRequiredAmount();
         for (uint i = 0; i < _merchants.length; i++) {
-            if (isMerchant[_merchants[i]]) revert NotAMerchant();
+            if (isMerchant[_merchants[i]]) revert NotAMerchant(_merchants[i]);
             if (_merchants[i] != address(0)) revert InvalidAddress();
         }
         Order[id] = order({
-            merchants: merchants,
+            merchants: _merchants,
             prices: prices,
         });
         return id;
     }
 
-    function PayMerchant(bytes8 id, bytes memory request, uint64 subscriptionId, uint32 gasLimit, bytes32 donID) external returns (bytes32 messageId) {
-        _sendRequest(request, subscriptionId, gasLimit, donID)
+    function requestPayment(bytes memory request, uint64 subscriptionId, uint32 gasLimit, bytes32 donID) external returns (bytes32 messageId) {
+       messageId = _sendRequest(request, subscriptionId, gasLimit, donID)
     }
 
     function fulfillRequest(bytes32 requestId, bytes memory response, bytes memory err) internal override {
-    
-    } 
+        string memory delivered = abi.decode(response);
+        
+    }
+
+    function PayMerchant(bytes8 id) internal {
+        orderDetails = order[id];
+        for (uint i = 0; i < orderDetails.merchants; i++) {
+            uint _amount = orderDetails.prices[i] * 1/100; // Takes 1% for commission 
+            orderDetails.prices[i] -= _amount;
+            Client.EVM2AnyMessage memory evm2AnyMessage = _buildCCIPMessage(
+                receiver: orderDetails.merchants[i],
+                token: usdcContract;
+                amount: orderDetails.prices[i],
+                feeToken: address(linkToken) // uses Link
+            );
+            uint256 fees = ccipRouter.getFee(chainSelector, evm2AnyMessage);
+            if (fees > linkToken.balanceOf(address(this))) revert NotEnoughFeesForGas(linkToken.balanceOf(address(this)), fees);
+
+            linkToken.approve(address(ccipRouter), fees);
+
+            usdc.approve(address(ccipRouter), orderDetails.prices[i]);
+
+            bytes32 messageId = ccipRouter.ccipSend(chainSelector, evm2AnyMessage);
+
+            emit Paid(messageId, receorderDetails.merchants[i], orderDetails.prices[i]);
+        }
+    }
 
     function checkCommission() external view onlyOwner returns (uint amount) {
         amount = commission[msg.sender];
-        return amount;
     }
 
     function withdrawCommission() external onlyOwner {
